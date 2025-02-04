a1eda54ec6f33fa9056df261a67d276b054f9c98
ZayBryx <zybryxm@gmail.com>
2025-02-04 16:42:58 +0800

    🎉

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+  ],
+});
